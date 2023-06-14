52817da1aaaa3229e859d3f9895b87810d2bbb33
aii <aii@devkeeper.ru>
2023-06-14 22:11:21 +0400

    fixed gitignoer

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+    public class PlayerController : MonoBehaviour
+{ 
+    private PlayerInput _playerControls;
+
+    private void OnEnable()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnDisable()
+    {
+        throw new NotImplementedException();
+    }
+}
