270a2903d1fd1f685b250342b36f9b331090146f
aii <aii@devkeeper.ru>
2023-06-14 23:26:21 +0400

    very base movement and jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,52 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
     public class PlayerController : MonoBehaviour
 { 
-    private PlayerInput _playerControls;
+    private Vector2 _moveDirection;
+    private float _moveSpeed = 1f;
+    private float _jumpValue;
+    private Rigidbody2D _rb;
+    private Vector2 _vecGravity;
+    private float _fallMultiplier = 30f;
 
-    private void OnEnable()
+    private void Start()
     {
-        throw new NotImplementedException();
+        _vecGravity = new Vector2(0, -Physics2D.gravity.y);
+        _rb = GetComponent<Rigidbody2D>();
+        
     }
 
-    private void OnDisable()
+    public void OnMove(InputAction.CallbackContext context)
     {
-        throw new NotImplementedException();
+        _moveDirection = context.ReadValue<Vector2>();
+    }
+    
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        _jumpValue = context.ReadValue<float>();
+    }
+    
+    private void Move(Vector2 direction)
+    {
+        var scaledMoveSpeed = _moveSpeed * Time.deltaTime;
+        transform.position += scaledMoveSpeed * new Vector3(direction.x, 0, 0);
+    }
+    
+    private void Jump(float jumpValue)
+    {
+        _rb.velocity = new Vector2(_rb.velocity.x, jumpValue * 10);
+        if (jumpValue == 0)
+        {
+            _rb.velocity -= _vecGravity * _fallMultiplier * Time.deltaTime;
+        }
+    }
+    
+    private void Update()
+    {
+        Move(_moveDirection);
+        Jump(_jumpValue);
     }
 }
